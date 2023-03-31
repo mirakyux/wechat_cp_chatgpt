64fe2c4420ebfe1c5674f55b080d16a0a46a8a29
mirakyux <mirakyux@163.com>
2023-04-01 01:22:47 +0800

    nothing

--- src/main/java/cn/mirakyux/wx_cp_bot/utils/SpringContextUtils.java
+++ src/main/java/cn/mirakyux/wx_cp_bot/utils/SpringContextUtils.java
@@ -4,11 +4,8 @@ import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.annotation.Nonnull;
 
 /**
  * SpringContextUtils
@@ -23,31 +20,12 @@ public class SpringContextUtils implements ApplicationContextAware {
     public SpringContextUtils() {
     }
 
-    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+    public void setApplicationContext(@Nonnull ApplicationContext applicationContext) throws BeansException {
         SpringContextUtils.applicationContext = applicationContext;
     }
 
     public static ApplicationContext getApplicationContext() {
         return applicationContext;
-    }
-
-    public static HttpServletRequest getHttpServletRequest() {
-        return ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
-    }
-
-    public static HttpServletResponse getHttpServletResponse() {
-        return ((ServletRequestAttributes)RequestContextHolder.getRequestAttributes()).getResponse();
-    }
-
-    public static String getDomain() {
-        HttpServletRequest request = getHttpServletRequest();
-        StringBuffer url = request.getRequestURL();
-        return url.delete(url.length() - request.getRequestURI().length(), url.length()).toString();
-    }
-
-    public static String getOrigin() {
-        HttpServletRequest request = getHttpServletRequest();
-        return request.getHeader("Origin");
     }
 
     public static Object getBean(String name) {
@@ -61,7 +39,7 @@ public class SpringContextUtils implements ApplicationContextAware {
     public static <T> T getBeanWithNullWhenError(Class<T> clazz) {
         try {
             return getApplicationContext().getBean(clazz);
-        } catch (Exception var2) {
+        } catch (Exception ignored) {
             return null;
         }
     }
